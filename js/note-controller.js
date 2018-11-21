fa09d52149703f65533eaebe8dd971ea1b94d0e4
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-11-21 12:04:51 +0000

    max 20 characters displayed in list

--- js/note-controller.js
+++ js/note-controller.js
@@ -1,20 +1,16 @@
-// function changeText(){
-//   document.getElementById("text").innerHTML = "Howdy";
-// }
 
 function Output(){
   var note = new Note
   document.getElementById("btn").onclick  = function() {
       var text = document.getElementById("user_input").value;
       note.addNote(text)
-      document.getElementById("note").innerHTML = text;
-      for (var I = 0; I < note.array.length; I++){
-       var noteList = "<li>" + note.array[I] + "</li>";
-       document.getElementById("note").innerHTML += noteList;
-      }
+      document.getElementById("note").innerHTML = "Notes:";
+        for (var I = 0; I < note.array.length; I++){
+         var noteList = "<li>" + note.array[I] + "</li>";
+         var note20 = noteList.substring(0,24)
+         document.getElementById("note").innerHTML += note20;
+        }
 
   }
-
-
 
 }
