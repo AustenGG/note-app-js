e5c2765dec33e394b3ba5787004f1aa8026de93c
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-11-22 11:40:55 +0000

    view full note

--- js/note-controller.js
+++ js/note-controller.js
@@ -1,4 +1,8 @@
-
+// // function Ready(){
+// //   document.addEventListener("DOMContentLoaded", function(){
+//       var note
+//   });
+// }
 function Output(){
   var note = new Note
   document.getElementById("btn").onclick  = function() {
@@ -6,11 +10,16 @@ function Output(){
       note.addNote(text)
       document.getElementById("note").innerHTML = "Notes:";
         for (var I = 0; I < note.array.length; I++){
-         var noteList = "<li>" + note.array[I] + "</li>";
+         var noteList = "<li>" + note.array[I] + "</li>" ;
          var note20 = noteList.substring(0,24)
          document.getElementById("note").innerHTML += note20;
-        }
-
+       }
   }
-
-}
+         document.getElementById("all").onclick = function(noteList){
+           document.getElementById("note").innerHTML = "Notes:";
+           for (var I = 0; I < note.array.length; I++){
+            var noteList = "<li>" + note.array[I] + "</li>" ;
+            document.getElementById("note").innerHTML = noteList;
+          }
+         }
+  }
